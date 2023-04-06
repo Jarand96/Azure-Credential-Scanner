4d39d30a30d8fba3e9878a261b178bac3f839be5
Jarand Nikolai Jansen <Jarandjansen182@gmail.com>
2023-04-06 23:41:41 +0200

    Scanner now includes all previous versions of the targeted logic apps.

--- logicapp_scanner.py
+++ logicapp_scanner.py
@@ -1,5 +1,4 @@
-import json
-import pprint
+import re
 import argparse
 import sys
 
@@ -8,19 +7,19 @@ from azure.identity import DefaultAzureCredential
 from azure.mgmt.logic import LogicManagementClient
 from azure.mgmt.resource import ResourceManagementClient
 
-
 # Look for key in dict
-def gen_dict_extract(key, var):
+def gen_dict_extract(word, var):
     if hasattr(var,'items'):
         for k, v in var.items():
-            if k.lower() == key:
+            if word.lower() in k.lower():
+            #if k.lower() == word:
                 yield v
             if isinstance(v, dict):
-                for result in gen_dict_extract(key, v):
+                for result in gen_dict_extract(word, v):
                     yield result
             elif isinstance(v, list):
                 for d in v:
-                    for result in gen_dict_extract(key, d):
+                    for result in gen_dict_extract(word, d):
                         yield result
 
 # Make wordlist from inputted file.
@@ -54,6 +53,41 @@ def find_words_in_objects(list_of_words, list_of_apps):
                 app['identified_creds'].append({word: temp_result})
     return list_of_objects_with_words
 
+# Check if credential has already been saved to logic app list of credentials.
+def is_cred_in_list(list_of_credentials, word, value):
+    for cred in list_of_credentials:
+        try:
+            # The credential is already in the saved list.
+            if str(cred.get(word)) == str(value):
+                return True
+        except:
+            # Something went wrong when trying to get credentials in list. 
+            return False
+    # Reached end of list, meaning credential is not in list. 
+    return False
+
+# Use this function to identified exposed creds in all versions of an app. 
+def find_words_in_app_versions(list_of_words, list_of_app_versions):
+
+    target_app = list_of_app_versions[0]
+    target_app['identified_creds'] = []
+
+    # Go through every version of the app
+    for app in list_of_app_versions:
+        # For every word in the wordlist
+        for i, word in enumerate(list_of_words):
+            #Look for word in app
+            result = gen_dict_extract(word,app)
+            temp_result = (list(result))
+            # If there is anything in the returned list, a secret was found
+            if len(temp_result) > 0:
+                # Check if that credential is already in the list, if not, save it in list. 
+                if not is_cred_in_list(target_app['identified_creds'], word, temp_result):
+                    target_app['identified_creds'].append({
+                        word: temp_result,
+                        'version' : app['name']
+                        })
+    return target_app
 
 # Take in command line arguments.  
 parser = argparse.ArgumentParser(description='Scan Azure subscription for dirty stuff')
@@ -72,7 +106,6 @@ SUBSCRIPTION_ID =  args.subscriptionid[0]
 
 if args.filename:
     try:
-        print(args.filename)
         listofwords = parse_wordlist(args.filename[0])
     except:
         listofwords = args.wordlist[0].split(',')
@@ -93,16 +126,40 @@ logic_client = LogicManagementClient(
 )
 # Get all workflows in defined subscription
 response = logic_client.workflows.list_by_subscription()
-
+logic_apps_with_credentials = []
 listofworkflows = []
 for item in response:
     listofworkflows.append(item.as_dict())
 
-logic_apps_with_credentials = find_words_in_objects(listofwords, listofworkflows)
+# Start working through each logic app.
+print("Looking for secrets in all previous versions of all logic apps in subscription: " + SUBSCRIPTION_ID)
+for app in listofworkflows:
+    # Extract resource group name from ID.
+    regex = "/resourceGroups\/([A-Za-z0-9\-\_]+)\/"
+    rgx_result = re.search(regex, app['id'])
+    rg_name = rgx_result.group(1)
+
+    # Get all versions for each logic app. 
+    response = logic_client.workflow_versions.list(
+    resource_group_name=rg_name,
+    workflow_name=app['name'],
+    )
+    # I am confused by response format. Make it dict. 
+    listofworkflowversions = []
+    for item in response:
+        listofworkflowversions.append(item.as_dict())
+    
+    # Search for secrets in all versions. 
+    searched_app = find_words_in_app_versions(listofwords, listofworkflowversions)
+    # If credentials was found. Add the app to list of dirty apps. 
+    if len(searched_app.get('identified_creds')) > 0:
+        # We need to remove duplicate credentials from different versions. 
+        app['identified_creds'] = searched_app['identified_creds']
+        logic_apps_with_credentials.append(app)
 
 if len(logic_apps_with_credentials) == 0:
     sys.exit("Could not find the specified words in any logic apps.")
-print("number of objects with credentials: " + str(len(logic_apps_with_credentials)))
+print("Number of objects with credentials: " + str(len(logic_apps_with_credentials)))
 print("These are the logic apps that contains credentials")
 for logicapp in logic_apps_with_credentials:
     print("\n")
